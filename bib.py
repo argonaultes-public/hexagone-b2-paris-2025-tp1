d9c233425f4172e69dd2012dbf3f009adb2badf2
argonaultes <gael@argonaultes.fr>
2025-02-10 15:27:04 +0100

    monolithic ok

--- bib.py
+++ bib.py
@@ -10,35 +10,44 @@ class App:
         self.__actions = {
         'ls': self.list_books,
         'new': self.new_book,
+        'd': self.delete_book,
+        'g': self.get_book,
+        's': self.save_to_disk,
         'q': self.should_continue
         }
         self.__book_store = BookStore()
         self.__library = Library()
+        self.__next_id = 0
 
     def should_continue(self):
         return False
 
     def list_books(self):
-        print('book_store.list()')
+        self.__book_store.list()
         return True
 
     def new_book(self):
         title = input('title: ')
         author = input('author: ')
         content = input('content: ')
-        book = Book(0, author, title, content)
+        book = Book(self.__next_id, author, title, content)
         self.__book_store.add(book)
+        self.__next_id += 1
         return True
 
     def delete_book(self):
+        book_id = int(input('Book id: '))
+        self.__book_store.delete(book_id)
         return True
 
     def get_book(self):
+        book_id = input('Book id: ')
+        book = self.__book_store.get(book_id)
+        print(book)
         return True
 
     def save_to_disk(self):
         # unpacking
-
         for filename, obj in [('my_lib.json', self.__library), ('my_book_store.json', self.__book_store)]:
             with open(filename, 'w') as lib_file:
                 raw_json = jsonpickle.encode(obj)
@@ -49,7 +58,10 @@ class App:
         should_continue = True
         while should_continue:
             action = input('Action? ')
-            should_continue = self.__actions[action]()
+            if action in self.__actions:
+                should_continue = self.__actions[action]()
+            else:
+                print(f'Action not supported {action}')
 
 if __name__ == '__main__':
     app = App()
--- models.py
+++ models.py
@@ -2,15 +2,41 @@
 class Book:
 
     def __init__(self, id, author, title, content):
-        self.__id = id #TODO: do not expose
+        self.__id = int(id)
         self.__title = title
         self.__author = author
         self.__content = content
 
+    @property
+    def id(self):
+        return self.__id
+
+    def __str__(self):
+        return f'{self.__id}: {self.__title}'
+
 class BookStore:
     
+    def __init__(self):
+        self.__books = set()
+
     def add(self, book):
-        pass
+        self.__books.add(book)
+
+    def delete(self, book_id):
+        book_to_remove = self.get(book_id)
+        self.__books.remove(book_to_remove)
+
+    def get(self, book_id):
+        book_result = None
+        for book in self.__books:
+            if book.id == book_id:
+                book_result = book
+                break
+        return book_result
+
+    def list(self):
+        for book in self.__books:
+            print(book)
 
 class Library:
     pass
