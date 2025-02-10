8bd18be2daef43674eb2619096ba93db93e5a5bf
argonaultes <gael@argonaultes.fr>
2025-02-10 12:49:54 +0100

    save_to_disk

--- bib.py
+++ bib.py
@@ -1,4 +1,3 @@
-import json
 import jsonpickle
 
 class Book:
@@ -26,6 +25,7 @@ class App:
         'new': self.new_book
         }
         self.__book_store = BookStore()
+        self.__library = Library()
 
 
     def list_books(self):
@@ -38,6 +38,19 @@ class App:
         book = Book(0, author, title, content)
         self.__book_store.add(book)
 
+    def delete_book(self):
+        pass
+
+    def get_book(self):
+        pass
+
+    def save_to_disk(self):
+        # unpacking
+
+        for filename, obj in [('my_lib.json', self.__library), ('my_book_store.json', self.__book_store)]:
+            with open(filename, 'w') as lib_file:
+                raw_json = jsonpickle.encode(obj)
+                lib_file.write(raw_json)
 
     def run(self):
         action = input('Action? ')
