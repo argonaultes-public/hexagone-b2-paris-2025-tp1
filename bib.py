3f805bb0b51c3c3e548004ae0f1560802620a9e8
argonaultes <gael@argonaultes.fr>
2025-02-10 11:23:47 +0100

    sample class creation

---
+++ bib.py
@@ -0,0 +1,15 @@
+class Livre:
+
+    def __init__(self):
+        self.__title = 'title'
+
+
+
+
+def process_trucs():
+    pass
+
+
+if __name__ == '__main__':
+    input_user = input('Que voulez-vous faire? ')
+    process_trucs()
